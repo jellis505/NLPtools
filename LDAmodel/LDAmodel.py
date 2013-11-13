1adef2b1617fe79a0e6773ca9e59b3f904269d7c
Joe <joe.ellis.505@gmail.com>
2013-11-13 18:30:26 -0500

    Finished LDA-based similarity search over our dataset

--- LDAmodel/LDAmodel.py
+++ LDAmodel/LDAmodel.py
@@ -16,11 +16,20 @@ saved_models_dir = "/ptvn/work/saved_lda_models"
 
 class LDAmodel():
 
-    def __init__(self, dictfile=None):
+    def __init__(self, modelfile=None, dictfile=None, corpusfile=None):
         # If we want to initialize from a pre-loaded dictionary file do it here
+        # Read in the dictionary file
         if dictfile != None:
             self.dict = corpora.Dictionary.load(dictfile)
-
+        
+        # Read in the modelfile
+        if modelfile !=None:
+            self.lda_model = ldamodel.LdaModel.load(modelfile)
+        
+         # Read in the modelfile
+        if corpusfile !=None:
+            self.corpus = corpora.mmcorpus.MmCorpus(corpusfile)
+        return 
 
     def ReadinFiles(self,directory):
         # This function reads in a directory of files and gets them into strings in a list for creation of a corpora
@@ -72,13 +81,19 @@ class LDAmodel():
         self.dict = bow_dict
         return bow_dict
 
-    def SaveDictionary(self,dictfile,dict=None):
+    def SaveDictionaryandCorpus(self,dictfile,corpusfile,dict=None,corpus=None):
         if dict == None:
             dict = self.dict
-
+        
+        if corpus == None:
+            corpus = self.corpus
+        
         # Saves the dictionary to a file for future reference
         dict.save(dictfile)
-
+        
+        # Save the corpora in Market Matrix format
+        corpora.mmcorpus.MmCorpus.serialize(corpusfile, corpus)
+        
         return
 
     def CreateLDAModel(self,topic_num):
@@ -108,10 +123,11 @@ class LDAmodel():
         tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
         texts = [[word for word in text if word not in tokens_once]
                  for text in texts]
-
+        
         dictionary = corpora.Dictionary(texts)
+        self.dict = dictionary
         corpus = [dictionary.doc2bow(text) for text in texts]
-            
+        self.corpus = corpus    
 
         # I can print out the documents and which is the most probable topics for each doc.
         lda = ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=num_t)
@@ -120,6 +136,27 @@ class LDAmodel():
         
         return lda
     
+    def ReturnSimilarArticles(self,document,num_returned=5):
+        # This function returns the indices of the documents that are supposed
+        # to be the most similar to our query
+        
+        # This gets the bag of word representation for our vocabulary for this topic
+        doc_bow = self.dict.doc2bow(document.lower().split())
+        print doc_bow
+        # Now let's find the most similar results
+        doc_lda = self.lda_model[doc_bow]
+        print doc_lda
+        
+        # Now let's create the similarity structure for these values
+        index = similarities.MatrixSimilarity(self.corpus)
+        
+        sims = index[doc_lda]
+        sims = sorted(enumerate(sims), key=lambda item: -item[1])
+        
+        results = sims[0:num_returned]
+        
+        return results
+        
     def SaveModel(self,saved_name,model=None):
         if model == None:
             model = self.lda_model
@@ -129,19 +166,22 @@ class LDAmodel():
 def run(argv):
     
     try:
-        opts, args = getopt.getopt(argv,'hct:n:')
+        opts, args = getopt.getopt(argv,'hct:n:r:')
     except getopt.GetoptError:
         print "Usage Error: Please see help"
         sys.exit(1)
     
     # This section parses the input variables to the script
     create_model = False
-    num_t = 20
+    num_t = 100
+    return_closest_articles = False
     for opt,arg in opts:
         if opt in ('-h'):
             print 'Help:'
             print '-c: Tells the program to create model file'
             print '-t: The topic number we want to process'
+            print '-n: The number of topics to generate from LDA'
+            print '-r: Tells us to return closes articles (default_number=5)'
         elif opt in ('-c'):
             create_model = True
         elif opt in ('-t'):
@@ -149,10 +189,15 @@ def run(argv):
             topic = int(topic_string)
         elif opt in ('-n'):
             num_t = int(arg)
+        elif opt in ('-r'):
+            return_closest_articles = True
+            document = arg
     
     # Create the files that we want to use
     topic_xml_file = os.path.join(topic_xml_dir,topic_string + ".topic")
     saved_model_file = os.path.join(saved_models_dir, topic_string + ".lda")
+    saved_dict_file = os.path.join(saved_models_dir, topic_string + ".dict")
+    saved_corpus_file = os.path.join(saved_models_dir, topic_string + ".mm")
     
     if create_model:
         content = reader.ReadArticlesforTopic(topic_xml_file)
@@ -162,8 +207,20 @@ def run(argv):
         lda_modeler = LDAmodel()
         lda = lda_modeler.TrainLDA(documents,num_t)
         lda_modeler.SaveModel(saved_model_file)
-        
+        lda_modeler.SaveDictionaryandCorpus(saved_dict_file,saved_corpus_file)
         print lda.show_topics()
+    
+    elif return_closest_articles:
+        lda_modeler = LDAmodel(saved_model_file,saved_dict_file, saved_corpus_file)
+        indices = lda_modeler.ReturnSimilarArticles(document,5)
+        
+        # Let's look at the title and description for each article based on the query
+        content = reader.ReadArticlesforTopic(topic_xml_file)
+        print indices
+        for index in indices:
+            print content[index[0]]
+        
+        
         
         
 if __name__ == "__main__":
